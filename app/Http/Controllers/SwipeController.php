7a39a570c7c6e7e7c24035d262c028c7081b402d
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-09-22 20:19:52 +0200

    Task 1 of Backend assessment Done

--- app/Http/Controllers/SwipeController.php
+++ app/Http/Controllers/SwipeController.php
@@ -12,17 +12,43 @@ class SwipeController extends Controller
     }
     public function swiperight($id,$id2)
     {
-        $userA=DB::table('users')->where('id','=',$id)->get()->toArray();
-        $userB=DB::table('users')->where('id','=',$id2)->get()->toArray();
-        //echo($user[0]->id);
-        echo($userA[0]->name).' likes: '.$userB[0]->name;
+        $userA=DB::table('users')->find($id);
+        $userB=DB::table('users')->find($id2);
+        $swipe=DB::table('users')->where('user_A','=',$id,'AND','user_B','=',$id2)->get()->toArray();
+        if(count($swipe)==0)
+        {
+            $swipe=DB::table('swipes')->where('user_A','=',$id2,'AND','user_B','=',$id)->get()->toArray();
+            if(count($swipe)==0)
+            {
+                DB::table('swipes')->insert([
+                    'user_A' => $id,
+                    'user_B' => $id2
+                ]);
+                echo($userA->name).' likes: '.$userB->name;
+            }
+            else
+            {
+                DB::table('swipes')->where('user_A','=',$id2,'AND','user_B','=',$id)->delete();
+                
+                DB::table('pairs')->insert([
+                    'user_A' => $id,
+                    'user_B' => $id2
+                ]);
+                echo($userA->name).' made a pair with: '.$userB->name;
+            }
+        }
+        else {
+            //Since we only have a table with 2 id rows of users, let's get rid of duplicates like this
+            echo 'Swipe already exists!';
+        }
+        
+        
     }
     public function swipeleft($id,$id2)
     {
-        $userA=DB::table('users')->where('id','=',$id)->get()->toArray();
-        $userB=DB::table('users')->where('id','=',$id2)->get()->toArray();
-        //echo($user[0]->id);
-        echo($userA[0]->name).' does not like: '.$userB[0]->name;
+        $userA=DB::table('users')->find($id);
+        $userB=DB::table('users')->find($id2);
+        echo($userA->name).' does not like: '.$userB->name;
     }
 
 }
